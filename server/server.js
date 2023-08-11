0414e640b66d8b73a27cb87f9ad165a3db5a81c2
Jojhan Silva <silvajojhan@gmail.com>
2023-08-10 20:46:16 -0500

    if in prod, use build folder in server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,10 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
