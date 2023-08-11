0e79a7a8db7387db38f8108bc79ccb8fecdd0e51
Jojhan Silva <silvajojhan@gmail.com>
2023-08-10 19:20:11 -0500

    added database connection to server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 
 const PORT = process.env.PORT || 3001;
+const db = require('./config/connection')
 
 const server = new ApolloServer({
   typeDefs,
