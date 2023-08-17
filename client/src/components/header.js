7e84481dd0ed2c4c3e2c662b8833e230b491c008
Jojhan Silva <silvajojhan@gmail.com>
2023-08-16 19:25:38 -0500

    added links to header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,11 +1,35 @@
-import React from 'react';
-
-function Header() {
-    return(
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+const Header = () => {
+    const [searchInput, setSearchInput] = useState('');
+    return (
         <header>
-            <h1>Header</h1>
+
+            <div>
+                <form>
+                    <input className="search-bar" type="text" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
+                    <Link to={`/search-results?search=${searchInput}`} className="custom-button" onClick={() => localStorage.setItem('searchInput', searchInput)} >
+                    </Link>
+                </form>
+
+                <div className="links">
+                    <Link to='/Profile' className='user-fav'>
+                        <div className="icon-container">
+                            <span className="icon-caption">Profile</span>
+                        </div>
+                    </Link>
+
+                    <Link to='/saved' className='user-fav'>
+                        <div className="icon-container">
+                            <span className="icon-caption">Saved</span>
+                        </div>
+
+                    </Link>
+                </div>
+            </div>
         </header>
     );
-}
+};
+
 
 export default Header;
